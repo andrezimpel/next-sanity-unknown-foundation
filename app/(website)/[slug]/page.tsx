683d1fedf5b71562a878695cddb907c14d9680b8
Andre Zimpel <andrezimpel@users.noreply.github.com>
2025-01-21 22:02:07 +0100

    Add dynamic page component for content rendering and metadata generation

---
+++ app/(website)/[slug]/page.tsx
@@ -0,0 +1,46 @@
+import { sanityFetch } from "@/sanity/lib/fetch"
+import { pageQuery } from "@/sanity/lib/queries"
+import { resolveOpenGraphImage } from "@/sanity/lib/utils"
+import { Metadata } from 'next'
+import { PortableText } from "next-sanity"
+import { PortableTextBlock } from "sanity"
+
+async function fetchPage({ params }: Props) {
+  return sanityFetch({
+    query: pageQuery,
+    params
+  })
+}
+
+type Props = {
+  params: Promise<{ slug: string }>
+}
+
+// Function to generate metadata
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const page = await fetchPage({ params })
+  const ogImage = resolveOpenGraphImage(page?.ogImage)
+
+  return {
+    ...(page?.title && { title: page?.title }),
+    ...(page?.metaDescription && { description: page.metaDescription }),
+    openGraph: {
+      ...(page?.ogTitle && { title: page.ogTitle }),
+      ...(ogImage && { images: [ogImage] }),
+    },
+    robots: {
+      index: !page?.noIndex
+    },
+  }
+}
+
+export default async function Page({ params }: Props) {
+  const page = await fetchPage({ params })
+
+  return (
+    <div className="container mx-auto space-y-6">
+      <h1 className="text-4xl font-bold">{page?.title}</h1>
+      <PortableText value={page?.content as PortableTextBlock[]} />
+    </div>
+  )
+}
