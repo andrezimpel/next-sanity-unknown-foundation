a48b5d27f4ad044ae84e1327a61a5ca5d9644371
Andre Zimpel <andrezimpel@users.noreply.github.com>
2025-01-25 22:53:35 +0100

    Replace unstable_cache with direct sanityFetch in TestPage
    - Removed unstable_cache for settings fetching
    - Updated sanityFetch call to include tags for revalidation
    - Simplified settings data retrieval logic

--- app/(test)/test/page.tsx
+++ app/(test)/test/page.tsx
@@ -1,18 +1,14 @@
 // import TestPageComponentAll from "./component-all"
 import { sanityFetch } from "@/sanity/lib/fetch"
 import { settingsQuery } from "@/sanity/lib/queries"
-import { unstable_cache } from 'next/cache'
 import TestPageComponentSeperate from "./component-seperate"
 
-const fetchSettings = unstable_cache(
-  async () => sanityFetch({
-    query: settingsQuery
-  }),
-  ['settings']
-)
-
 export default async function TestPage() {
-  const settings = await fetchSettings()
+  const settings = await sanityFetch({
+    query: settingsQuery,
+    tags: ["settings"],
+  })
+
   return (
     <div>
       <div>Test Page intself: {settings?.title}</div>
